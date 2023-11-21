b349aa710860fc6eab49a8527373ad4bd3b010a0
Arbu919 <124624961+Arbu919@users.noreply.github.com>
2023-11-21 23:58:58 +0500

    user and video models

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
-
 import connectDB from "./db/db.js";
-
 dotenv.config({
     path: './env',
 })
 
 connectDB()
 .then(() => {
-    app.on('error',(e)=>{
-        console.log(`Error: ${e}`);
-    })
     app.listen(process.env.PORT || 8000, () => {
        console.log(`server is running at port : ${process.env.PORT}`) 
     })
 })
 .catch((err) => {
-    console.log("mongodb connection error!!! ", err)
+    console.log("PORT connection error>> ", err)
 })
